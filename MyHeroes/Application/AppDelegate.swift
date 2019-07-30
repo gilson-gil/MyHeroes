dc74537348be596f9e54f4cb2323280cc5a1bc1f
Gilson Takaasi Gil <gilson.gil@me.com>
2019-07-30 19:03:49 -0300

    Initial commit

---
+++ MyHeroes/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MyHeroes
+//
+//  Created by Gilson Takaasi Gil on 30/07/19.
+//  Copyright © 2019 Gilson Gil. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
